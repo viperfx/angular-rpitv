dc46fa3af40d2de4dfb4bf1f81524265c68b9d85
Tharshan <tharshan09@gmail.com>
2013-06-11 00:08:11 +0100

    merged toogles app with socket.io+angular seed repo
    Added socket handler code from RPiTV project

--- app.js
+++ app.js
@@ -5,19 +5,22 @@
 
 var express = require('express'),
   routes = require('./routes'),
-  socket = require('./routes/socket.js');
+  socket = require('./routes/socket.js'),
+  app = module.exports = express(),
+  server = require('http').createServer(app)
+, https = require('https')
+, path = require('path')
+, io = require('socket.io').listen(server)
+, spawn = require('child_process').spawn
+, omx = require('omxcontrol');
 
-var app = module.exports = express();
-var server = require('http').createServer(app);
-
-// Hook Socket.io into Express
-var io = require('socket.io').listen(server);
 
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'html');
+  app.engine('html', require('ejs').renderFile);  
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(__dirname + '/public'));
@@ -42,7 +45,92 @@ app.get('*', routes.index);
 
 // Socket.io Communication
 
-io.sockets.on('connection', socket);
+//Run and pipe shell script output
+function run_shell(cmd, args, cb, end) {
+    var spawn = require('child_process').spawn,
+        child = spawn(cmd, args),
+        me = this;
+    child.stdout.on('readable', function () { cb(me, child.stdout); });
+    child.stdout.on('end', end);
+}
+var ss;
+//Socket.io Server
+io.sockets.on('connection', function (socket) {
+
+ socket.on("screen", function(data){
+   socket.type = "screen";
+   ss = socket;
+   console.log("Screen ready...");
+ });
+ socket.on("remote", function(data){
+   socket.type = "remote";
+   console.log("Remote ready...");
+ });
+
+ socket.on("controll", function(data){
+  console.log(data);
+  if(socket.type === "remote"){
+
+   if(data.action === "tap"){
+     if(ss != undefined){
+      ss.emit("controlling", {action:"enter"});
+    }
+  }
+  else if(data.action === "swipeLeft"){
+    if(ss != undefined){
+      ss.emit("controlling", {action:"goLeft"});
+    }
+  }
+  else if(data.action === "swipeRight"){
+   if(ss != undefined){
+     ss.emit("controlling", {action:"goRight"});
+   }
+ }
+}
+});
+ socket.on("log", function(data){
+  console.log(data);
+});
+ socket.on("video", function(data){
+
+  if( data.action === "play"){
+    console.log(data.video_id);
+    var id = data.video_id,
+    url = "http://www.youtube.com/watch?v="+id;
+    var runShell = new run_shell('youtube-dl',['-gf', '18/22/34/35/37', url],
+      function (me, stdout) { 
+                //console.log(escape(stdout.read().toString().replace(/[\r\n]/g, "")));
+                me.stdout = stdout.read().toString().replace(/[\r\n]/g, "");
+                socket.emit("loading",{output: me.stdout});
+                omx.start(me.stdout);
+              }, 
+              function (me) {
+                console.log("Finished");
+              });
+  }
+  if( data.action == "stream") {
+    var id = data.video_id, 
+    url = "https://api.put.io/v2/files/"+id+"/mp4/stream/?oauth_token=Z7ARQSU2";
+    var options = {
+      host: 'api.put.io',
+      port: 443,
+      path: '/v2/files/'+id+'/stream?oauth_token=Z7ARQSU2',
+      method: 'GET'
+    };
+    
+    var req = https.request(options, function(res) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      omx.start(res.headers.location);
+    });
+    req.end();
+    req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+    });
+  }
+
+});
+});
 
 // Start server
 
---
+++ public/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Angular setup
+var tooglesApp = angular.module('tooglesApp', ['ngSanitize'])
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/browse', { templateUrl: 'partials/list', controller: 'ListCtrl' });
+    $routeProvider.when('/browse/:category', { templateUrl: 'partials/list', controller: 'ListCtrl' });
+    $routeProvider.when('/search/:query', { templateUrl: 'partials/list', controller: 'ListCtrl' });
+    $routeProvider.when('/view/:id', { templateUrl: 'partials/view', controller: 'ViewCtrl' });
+    $routeProvider.when('/playlist/:id', { templateUrl: 'partials/view', controller: 'ViewCtrl' });
+    $routeProvider.when('/playlist/:id/:start', { templateUrl: 'partials/view', controller: 'ViewCtrl' });
+    $routeProvider.when('/user/:username', { templateUrl: 'partials/list', controller: 'ListCtrl' });
+    $routeProvider.when('/user/:username/:feed', { templateUrl: 'partials/list', controller: 'ListCtrl' });
+    $routeProvider.otherwise({ redirectTo: '/browse' });
+    // $locationProvider.html5Mode(true);
+  }]);
